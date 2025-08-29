d40cc17a71a09fbf0936c551e089085e53bac247
Alexander Andersen <157651781+AndersenAlexander@users.noreply.github.com>
2025-08-29 20:59:20 +0200

    Add files via upload

---
+++ hw02-express/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import morgan from "morgan";
+import contactsRouter from "./routes/contactsRouter.js";
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+// routes
+app.use("/api/contacts", contactsRouter);
+
+// 404
+app.use((req, res) => {
+  res.status(404).json({ message: "Not found" });
+});
+
+// error handler
+app.use((err, req, res, _next) => {
+  const status = err.status || 500;
+  res.status(status).json({ message: err.message || "Server error" });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`REST API on http://localhost:${PORT}`));
